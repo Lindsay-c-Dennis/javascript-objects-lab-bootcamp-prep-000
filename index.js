ddbfa69e8e8f71face0990281cac779802af47ea
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2017-12-03 03:38:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ function updateObjectWithKeyAndValue (object, key, value){
   recipes.key = 'value'
   return recipes
 }
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  recipes
+}
